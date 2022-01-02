d40bfb33c626f3754259ae56016b1704fcd30db0
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2022-01-02 12:44:10 +0530

    Create Kth Smallest Factor.cpp

---
+++ Kth Smallest Factor.cpp
@@ -0,0 +1,28 @@
+// link - https://practice.geeksforgeeks.org/problems/kth-smallest-factor2345/1#
+
+int kThSmallestFactor(int N , int K) {
+		int count =0;
+        vector<int> ans;
+        int ans1=0;
+        
+        for (int i=1; i<=N; i++){
+            if (N%i==0){
+                
+                ans.push_back(i);
+                count++; 
+            }
+        }
+        
+        if ((K-1)<count){
+            ans1 = ans[K-1];
+        }
+        
+        else {
+            ans1= -1;
+        }
+        
+        return ans1;
+
+
+ 		}
+};
