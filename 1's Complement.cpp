c86b0a053ab3c4186343aa047485143e1cc71513
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2022-01-07 21:52:13 +0530

    Create 1's Complement.cpp

---
+++ 1's Complement.cpp
@@ -0,0 +1,26 @@
+// link - https://practice.geeksforgeeks.org/problems/1s-complement2819/1
+
+class Solution{ 
+public:
+    string onesComplement(string S,int N){
+        //code here
+        
+        //string temp ="";
+        
+        for (int i=0; i<S.length(); i++){
+            
+            if (S[i]=='1'){
+                
+                S[i]= '0';
+                
+            }
+            
+            else {
+                S[i]= '1';
+            }
+        }
+        
+        return S;
+        
+    }
+};
